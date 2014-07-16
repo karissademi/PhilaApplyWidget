08dbee37c98b0c5afae177420cb3823a3f5c11ba
Karissa <k@karissademi.com>
2014-07-16 16:48:23 -0400

    added styles

--- PhilaObtainWidget.php
+++ PhilaObtainWidget.php
@@ -15,8 +15,8 @@ $message = <<<EOM
     <h1 class="PhilaWidgetTitle">Obtain</h1>
         <div id="PhilaObtainLinkBlock">
 			<div class="PhilaObtainDropDown">
-				<select rows="2" name="PhilaLicenceJumpMenu" id="PhilaLicenceJumpMenu" onchange="javascript:document.location = this.value;">
-					<option value="#">Licences</option>
+				<select rows="2" name="PhilaLicenceJumpMenu" id="PhilaLicenceJumpMenu" onchange="javascript:document.location = this.value;" class="pick-me" data-class="licence-drop-down">
+					<option value="">Licences</option>
                             <option value="http://www.acctphilly.org/">Animal License</option>
                             <option value="https://business.phila.gov/Pages/Result.aspx?stage=Start&type=Construction&section=Licensing%20%26%20Regulations">Building & Construction</option>
                             <option value="https://business.phila.gov/Pages/Result.aspx?stage=Start&type=All%20Business%20Types&section=Licensing%20%26%20Regulations">Business</option>
@@ -25,8 +25,8 @@ $message = <<<EOM
                             <option value="http://secureprod.phila.gov/wills/marriagelicense.aspx">Marriage License</option>
                             <option value="http://www.philadelphiastreets.com/index.php/complete-streets-handbook">Street Closure</option>
                 </select>
-                <select rows="2" name="PhilaPermitJumpMenu" id="PhilaPermitJumpMenu" onchange="javascript:document.location = this.value;">
-                        <option value="#">Permits</option>
+                <select rows="2" name="PhilaPermitJumpMenu" id="PhilaPermitJumpMenu" onchange="javascript:document.location = this.value;" class="pick-me" data-class="permit-drop-down">
+                        <option value="">Permits</option>
                             <option value="http://www.phila.gov/health/AirManagement/AirPermittingForms.html">Air Pollution</option>
                             <option value="http://www.phila.gov/health/AirManagement/AsbestosControl.html">Asbestos Control</option>
                             <option value="http://www.phila.gov/MDO/SpecialEvents/Pages/PermitsandApplications.aspx">Block Party</option>
@@ -39,8 +39,8 @@ $message = <<<EOM
                             <option value="https://secure.phila.gov/Streets/HaulingPermit/">Special Hauling Permit</option>
                             <option value="http://www.phila.gov/health/permitsForms.html">Tobacco Retailer Permit</option>
                 </select>
-                <select rows="2" name="PhilaCertificateJumpMenu" id="PhilaCertificateJumpMenu" onchange="javascript:document.location = this.value;">
-                        <option value="#">Certificates</option>
+                <select rows="2" name="PhilaCertificateJumpMenu" id="PhilaCertificateJumpMenu" onchange="javascript:document.location = this.value;" class="pick-me" data-class="certificate-drop-down">
+                        <option value="">Certificates</option>
                             <option value="http://www.portal.state.pa.us/portal/server.pt/community/birth_certificates/14121">Birth Certificate</option>
                             <option value="https://business.phila.gov/Pages/Result.aspx?stage=Start&type=All%20Business%20Types&section=Permits%20%26%20Certificates">Business</option>
                             <option value="http://www.portal.state.pa.us/portal/server.pt/community/death_certificates/14122">Death Certificate</option>
